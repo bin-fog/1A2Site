06d3a7f1364c9e20bd4dea20922c7acc5bc1c3a0
Mrksist <platon_fil08@mail.ru>
2023-11-08 01:54:54 +0300

    Несу фронт домой в 2 ночи

---
+++ components/Main.js
@@ -0,0 +1,134 @@
+export default function Main() {
+  return (
+    <section class="text-gray-600 body-font">
+      <div class="max-w-5xl pt-52 pb-24 mx-auto">
+        <h1 class="mazzardH text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
+          НЕЙМАРК.Абитур
+        </h1>
+        <h2 class="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
+          Портал для абитуриентов и студентов,
+          <br/>
+          заинтересованных экосистемой НЕЙМАРК
+        </h2>
+        <div className="ml-6 text-center">
+          <a
+            className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-neyred-100 focus:shadow-outline"
+            href="/"
+          >
+            <div className="flex text-lg">
+              <span className="justify-center">Зарегистрироваться</span>
+            </div>
+          </a>
+          <a
+            className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-neyred-100 focus:shadow-outline ml-2"
+            href="/"
+          >
+            <div className="flex text-lg">
+              <span className="justify-center">Войти</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <h2 className="mb-1 text-2xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl">
+        Удобный и быстрый сервис
+      </h2>
+      <br></br>
+      <p className="mx-auto text-xl text-center text-gray-300 font-normal leading-relaxed fs521 lg:w-2/3">
+        Мы учитываем ваши достижения и предпочтения, на их основе подбираем мероприятия, которые Вам, вероятнее всего, подойдут
+        <br />
+        Также наш сервис может помочь поступить в ИТ-кампус НЕЙМАРК на наиболее подходящее Вам направление.
+        <br />
+        Мы предлагаем ознакомиться с ближайшими запланированными мероприятиями:
+      </p>
+      <div className="pt-12 pb-24 max-w-4xl mx-auto fsac4 md:px-1 px-3">
+        <div class="ktq4">
+          <a href="#">
+          <h3 class="pt-3 font-semibold text-lg text-white">
+            МОЯ ПРОФЕССИЯ - ИТ 2023
+          </h3>
+          <p class="pt-2 value-text text-md text-gray-200 ">
+            Это хакатон, на котором вам будут много чего обещать,
+            в том числе поселение в роскошном отеле посреди 
+            ИТ-столицы России, по факту, сбудется из этого лишь
+            половина.
+          </p>
+          </a>
+        </div>
+        <div class="ktq4">
+          <h3 class="pt-3 font-semibold text-lg text-white">
+            Lorem ipsum dolor sit amet
+          </h3>
+          <p class="pt-2 value-text text-md text-gray-200">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
+            porttitor.
+          </p>
+        </div>
+        <div class="ktq4">
+          <h3 class="pt-3 font-semibold text-lg text-white">
+            Lorem ipsum dolor sit amet
+          </h3>
+          <p class="pt-2 value-text text-md text-gray-200">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
+            porttitor.
+          </p>
+        </div>
+        <div class="ktq4">
+          <h3 class="pt-3 font-semibold text-lg text-white">
+            Lorem ipsum dolor sit amet
+          </h3>
+          <p class="pt-2 value-text text-md text-gray-200">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            tincidunt a libero in finibus. Maecenas a nisl vitae ante rutrum
+            porttitor.
+          </p>
+        </div>
+      </div>
+      <h2 className="mb-1 text-2xl font-semibold tracking-tighter text-center text-gray-200 lg:text-7xl md:text-6xl">
+        Для поступающих
+      </h2>
+      <p className="mx-auto text-xl text-center text-gray-300 font-normal leading-relaxed fs521 pt-10 lg:w-2/3">
+        Поступающим в ИТ-кампус следует заполнить следующую анкету:
+      </p>
+      <div className="ml-6 pb-10 text-center pt-10">
+          <a
+            className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-neyred-100 focus:shadow-outline"
+            href="/anket"
+          >
+            <div className="flex text-lg">
+              <span className="justify-center">Заполнить</span>
+            </div>
+          </a>
+        </div>
+        <div className="pb-12 max-w-6xl mx-auto fsac4 md:px-1 px-3">
+        <div class="ktq4">
+          <img src="https://nine4.app/images/nine4-3.png"></img>
+          <h3 class="pt-3 font-semibold text-lg text-white">
+            Lorem ipsum dolor sit amet
+          </h3>
+          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
+            Fusce pharetra ligula mauris, quis faucibus lectus elementum vel.
+            Nullam vehicula, libero at euismod tristique, neque ligula faucibus
+            urna, quis ultricies massa enim in nunc. Vivamus ultricies, quam ut
+            rutrum blandit, turpis massa ornare velit, in sodales tellus ex nec
+            odio.
+          </p>
+        </div>
+        <div class="ktq4">
+          <img src="https://nine4.app/images/nine4-3.png"></img>
+          <h3 class="pt-3 font-semibold text-lg text-white">
+            Lorem ipsum dolor sit amet
+          </h3>
+          <p class="pt-2 value-text text-md text-gray-200 fkrr1">
+            Fusce pharetra ligula mauris, quis faucibus lectus elementum vel.
+            Nullam vehicula, libero at euismod tristique, neque ligula faucibus
+            urna, quis ultricies massa enim in nunc. Vivamus ultricies, quam ut
+            rutrum blandit, turpis massa ornare velit, in sodales tellus ex nec
+            odio.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
