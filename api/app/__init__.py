7cd65776cac2b7aa4b116190dcfcc87eeae088f6
lev <robocat2020lev@gmail.com>
2023-11-08 01:14:08 +0300

    models update

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-from config import Config
+from app.config import Config
 
 app = Flask(__name__)
-CORS(app)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
